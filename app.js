62635d63148bc0674db09529adad12ca8535c138
JeongHyeon Park <9193jane@gmail.com>
2024-01-08 18:23:36 +0900

    20240108 5.5 Express 정적파일

--- app.js
+++ app.js
@@ -1,11 +1,16 @@
 var express = require('express');
 var app = express();
+app.use(express.static('public'));
 app.get('/',  function (req, res) {
     res.send('Hello Home Page🏡')
 });
+app.get('/route',  function (req, res) {
+    res.send('Hello Kermit the Frog <img src = "/Kermit.jpeg">')
+});
+
 app.get('/login', function (req, res) {
     res.send('Login Plz🙏')
-})
+});
 app.listen(3000, function () {
     console.log('Connected 3000 port!');
 });
--- webserver2.js
+++ webserver2.js
@@ -5,7 +5,7 @@ const port = 3000;
 
 http.createServer((req, res) => {
     res.writeHead(200, {'Content-Type' : 'text/plain'});
-    res.end('Hello World\n');
+    res.end('Hello World\n Web Server2');
 }).listen(port, hostname, () => {
     console.log(`Server running at http://${hostname}:${port}/`);
 });
