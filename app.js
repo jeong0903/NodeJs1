1d75a826da07907b70fa3a25a2aa7c440b3ebd38
JeongHyeon Park <9193jane@gmail.com>
2024-01-08 19:08:01 +0900

    20240108 5.6 Express WebPage

--- app.js
+++ app.js
@@ -4,6 +4,30 @@ app.use(express.static('public'));
 app.get('/',  function (req, res) {
     res.send('Hello Home Page🏡')
 });
+app.get('/dynamic',  function (req, res) {
+    var lis = '';
+    for (var i = 0; i < 5; i++) {
+        lis = lis + '<li>coding</li>';
+    }
+    var time = Date();
+    var output = `
+    <!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        Hello Dynamic
+        <ul>
+            ${lis}
+        </ul>
+        ${time}
+    </body>
+    </html>`
+    res.send(output);
+});
 app.get('/route',  function (req, res) {
     res.send('Hello Kermit the Frog <img src = "/Kermit.jpeg">')
 });
