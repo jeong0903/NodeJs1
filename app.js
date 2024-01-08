075faaf4c1715448937ea9b3608084ca77e9b16e
JeongHyeon Park <9193jane@gmail.com>
2024-01-08 23:08:56 +0900

    20240108 8.3 Express Post Body-parser

--- app.js
+++ app.js
@@ -1,9 +1,11 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 var app = express();
 app.locals.pretty = true;
 app.set('view engine', 'pug');
 app.set('views', './views')
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: false}))
 app.get('/form', function (req, res) {
     res.render('form');
 })
@@ -11,6 +13,11 @@ app.get('/form_receiver', function (req, res) {
     var title = req.query.title;
     var description = req.query.description;
     res.send(title+', '+description);
+})
+app.post('/form_receiver', function (req, res) {
+    var title = req.body.title;
+    var description = req.body.description;
+    res.send(`${title}, ${description}`);
 })
 app.get('/topic/:id', function (req, res) {
     var topics = [
