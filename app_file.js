53e6ac4e0a45af38e806c010193453affd219bc2
JeongHyeon Park <9193jane@gmail.com>
2024-01-09 17:24:56 +0900

    20240109 9.4 글목록(topics)

--- app_file.js
+++ app_file.js
@@ -1,25 +1,34 @@
-var express = require('express');
-var bodyParser = require('body-parser');
-var fs = require('fs');
+var express = require("express");
+var bodyParser = require("body-parser");
+var fs = require("fs");
 var app = express();
-app.use(bodyParser.urlencoded({extended:false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.locals.pretty = true;
-app.set('views', './views_file');
-app.set('view engine', 'pug');
-app.get('/topic/new', function (req, res) {
-    res.render('new')
-})
-app.post('/topic', function (req, res) {
-    var title = req.body.title;
-    var description = req.body.description;
-    fs.writeFile('data/'+title, description, function (err) {
+app.set("views", "./views_file");
+app.set("view engine", "pug");
+app.get("/topic/new", function (req, res) {
+  res.render("new");
+});
+app.get('/topic', function (req, res) {
+    fs.readdir('data', function (err, files) {
         if (err) {
             console.log(err);
-            res.status(500).send('Interneal Server Error');            
+            res.status(500).send('Internal Server Error');
         }
-        res.send('Success!');
-    });
+        res.render('view', {topics:files});
+    })
 })
+app.post("/topic", function (req, res) {
+  var title = req.body.title;
+  var description = req.body.description;
+  fs.writeFile("data/" + title, description, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(500).send("Interneal Server Error");
+    }
+    res.send("Success!");
+  });
+});
 app.listen(3000, function () {
-    console.log('Conneted, 3000 port!');
-})
+  console.log("Conneted, 3000 port!");
+});
