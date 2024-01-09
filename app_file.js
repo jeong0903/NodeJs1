30101820420210a69fa904fe35cc0eff9bcae37b
JeongHyeon Park <9193jane@gmail.com>
2024-01-09 18:24:28 +0900

    20240109 9.6 refactoring

--- app_file.js
+++ app_file.js
@@ -7,34 +7,42 @@ app.locals.pretty = true;
 app.set("views", "./views_file");
 app.set("view engine", "pug");
 app.get("/topic/new", function (req, res) {
-  res.render("new");
-});
-app.get("/topic", function (req, res) {
   fs.readdir("data", function (err, files) {
     if (err) {
       console.log(err);
       res.status(500).send("Internal Server Error");
     }
-    res.render("view", { topics: files });
+    res.render("new", {topics: files});
   });
 });
-app.get("/topic/:id", function (req, res) {
-  var id = req.params.id;
+app.get(["/topic", "/topic/:id"], function (req, res) {
+  // topics 정보
   fs.readdir("data", function (err, files) {
     if (err) {
       console.log(err);
       res.status(500).send("Internal Server Error");
     }
+    var id = req.params.id;
+    if (id) {
+      // id값이 있을 때
+      fs.readFile("data/" + id, "utf8", function (err, data) {
+        if (err) {
+          console.log(err);
+          res.status(500).send("Internal Server Error");
+        }
+        res.render("view", { topics: files, title: id, description: data });
+      });
+    } else {
+      // id값이 없을 때
+      res.render("view", {
+        topics: files,
+        title: "Welcome",
+        description: "Hello world!",
+      });
+    }
+  });
+});
 
-    fs.readFile("data/" + id, "utf8", function (err, data) {
-      if (err) {
-        console.log(err);
-        res.status(500).send("Internal Server Error");
-      }
-      res.render("view", { topics: files , title: id, description: data});
-    });
-  });
-});
 app.post("/topic", function (req, res) {
   var title = req.body.title;
   var description = req.body.description;
@@ -43,7 +51,7 @@ app.post("/topic", function (req, res) {
       console.log(err);
       res.status(500).send("Internal Server Error");
     }
-    res.send("Success!");
+    res.redirect("/topic/"+title);
   });
 });
 app.listen(3000, function () {
