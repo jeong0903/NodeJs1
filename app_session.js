aab20213609353072b774a81645400be7df635e0
JeongHyeon Park <9193jane@gmail.com>
2024-01-16 15:59:29 +0900

    20240116 2.4.login1

--- app_session.js
+++ app_session.js
@@ -11,12 +11,29 @@ app.use(
 );
 
 app.get("/count", function (req, res) {
-    if(req.session.count){
-        req.session.count++;
-    } else{
-        req.session.count = 1;
-    }
+  if (req.session.count) {
+    req.session.count++;
+  } else {
+    req.session.count = 1;
+  }
   res.send("count : " + req.session.count);
+});
+
+app.get("/auth/login", function (req, res) {
+  var output = `
+  <form action="/auth/login" method="post">
+    <p>
+      <input type="text" name="username" placeholder="user name">
+    </p>
+    <p>
+      <input type="password" name="password" placeholder="password">
+    </p>
+    <p>
+      <input type="submit">
+    </p>
+  </form>
+  `;
+  res.send(output);
 });
 
 app.listen(3000, function () {
