ead0b8fcf178707c51f4b77d06e5e9879219898d
JeongHyeon Park <9193jane@gmail.com>
2024-01-17 00:31:04 +0900

    20240116 4.3.route

--- app_passport_file.js
+++ app_passport_file.js
@@ -3,8 +3,8 @@ var session = require("express-session");
 var FileStore = require("session-file-store")(session);
 var bodyParser = require("body-parser");
 var pbkdf2 = require("pbkdf2-password");
-var passport = require('passport')
-var localStrategy = require('passport-local').Strategy
+var passport = require("passport");
+var LocalStrategy = require("passport-local").Strategy;
 var hasher = pbkdf2();
 
 var app = express();
@@ -68,43 +68,91 @@ app.get("/auth/login", function (req, res) {
   res.send(output);
 });
 
-app.post("/auth/login", function (req, res) {
-  var uname = req.body.username;
-  var pwd = req.body.password;
+passport.serializeUser(function (user, done) {
+  console.log('serializeUser', user);
+  done(null, user.username);
+})
+passport.deserializeUser(function (id, done) {
+  console.log('deserializeUser', id);
   for (var i = 0; i < users.length; i++) {
     var user = users[i];
-    if(uname === user.username){
-      return hasher({password:pwd, salt: user.salt, }, function (err, pass, salt, hash) {
-        if(hash === user.password){
-          req.session.displayName = user.displayName;
-          req.session.save(function () {
-            res.redirect('/welcome')
-          })
-        }else{
-          res.send('who are you? <a href ="/auth/login">log in</a>')
-        }
-      })
-    }
-    // if (uname === user.username && pbkdf2(pwd+user.salt) === user.password) {
-    //   req.session.displayName = user.displayName;
-    //   return req.session.save(function () {
-    //     res.redirect("/welcome");
-    //   });
-    // }
+    if(user.username === id){
+      return done(null, user);
+    }    
   }
-  res.send('who are you? <a href ="/auth/login">log in</a>')
 });
+
+passport.use(
+  new LocalStrategy(function (username, password, done) {
+    var uname = username;
+    var pwd = password;
+    for (var i = 0; i < users.length; i++) {
+      var user = users[i];
+      if (uname === user.username) {
+        return hasher(
+          { password: pwd, salt: user.salt },
+          function (err, pass, salt, hash) {
+            if (hash === user.password) {
+              console.log('LocalStrategy', user);
+              done(null, user);
+            } else {
+              done(null, false);
+            }
+          }
+        );
+      }
+    }
+    done(null, false)
+  })
+);
+app.post(
+  "/auth/login",
+  passport.authenticate("local", {
+    successRedirect: "/welcome",
+    failureRedirect: "/auth/login",
+    failureFlash: false,
+  })
+);
+
+// app.post("/auth/login", function (req, res) {
+//   var uname = req.body.username;
+//   var pwd = req.body.password;
+//   for (var i = 0; i < users.length; i++) {
+//     var user = users[i];
+//     if(uname === user.username){
+//       return hasher({password:pwd, salt: user.salt, }, function (err, pass, salt, hash) {
+//         if(hash === user.password){
+//           req.session.displayName = user.displayName;
+//           req.session.save(function () {
+//             res.redirect('/welcome')
+//           })
+//         }else{
+//           res.send('who are you? <a href ="/auth/login">log in</a>')
+//         }
+//       })
+//     }
+//     // if (uname === user.username && pbkdf2(pwd+user.salt) === user.password) {
+//     //   req.session.displayName = user.displayName;
+//     //   return req.session.save(function () {
+//     //     res.redirect("/welcome");
+//     //   });
+//     // }
+//   }
+//   res.send('who are you? <a href ="/auth/login">log in</a>')
+// });
+
 // var salt = 'asdf1234!@#$'
 var users = [
   {
     username: "jane",
-    password: 'wy3aXKhBRVcvyLzi3xZiecralO0/836dObMFbg4ZddLTcsOmipka7HSPRarPXryoU4la4OIjVaP8N8jw0vG1A/YenoNDFMjk+eL2B09K0tm4EzIJ0eqrbuYka84wvYwh59qBjttf1TELqWEVy50mVJaFFQiQUQh43bkutFVUPtg=',
-    salt: 'v4rZzwL2+e2A917wNDHuWbbV1h35CtGtO6b/P4CgnL6bwpviQ8ilA7qkwY2+S1DX+Fu0O4BxWwXMnjzwfHkJFA==',
+    password:
+      "wy3aXKhBRVcvyLzi3xZiecralO0/836dObMFbg4ZddLTcsOmipka7HSPRarPXryoU4la4OIjVaP8N8jw0vG1A/YenoNDFMjk+eL2B09K0tm4EzIJ0eqrbuYka84wvYwh59qBjttf1TELqWEVy50mVJaFFQiQUQh43bkutFVUPtg=",
+    salt: "v4rZzwL2+e2A917wNDHuWbbV1h35CtGtO6b/P4CgnL6bwpviQ8ilA7qkwY2+S1DX+Fu0O4BxWwXMnjzwfHkJFA==",
     displayName: "Jane",
   },
 ];
 app.post("/auth/register", function (req, res) {
-  hasher({password: req.body.password}, function (err, pass, salt, hash) {
+  hasher({ password: req.body.password }, function (err, pass, salt, hash) {
     var user = {
       username: req.body.username,
       password: hash,
@@ -116,9 +164,9 @@ app.post("/auth/register", function (req, res) {
     req.session.save(function () {
       res.redirect("/welcome");
     });
-  })
+  });
 });
-app.get('/auth/register', function (req, res) {
+app.get("/auth/register", function (req, res) {
   var output = `
   <h1>REGISTER</h1>
   <form action="/auth/register" method="post">
@@ -137,7 +185,7 @@ app.get('/auth/register', function (req, res) {
   </form>
   `;
   res.send(output);
-})
+});
 app.listen(3000, function () {
   console.log("Connected 3000 port is running!");
 });
